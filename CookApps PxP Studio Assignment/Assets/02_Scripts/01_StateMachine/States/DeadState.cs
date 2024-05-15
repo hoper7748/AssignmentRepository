432c47eed2901eb8167772b09a733361d518dd74
JHSeong <50869470+hoper7748@users.noreply.github.com>
2024-05-15 23:20:10 +0900

    Add
    Unitask

--- CookApps PxP Studio Assignment/Assets/02_Scripts/01_StateMachine/States/DeadState.cs
+++ CookApps PxP Studio Assignment/Assets/02_Scripts/01_StateMachine/States/DeadState.cs
@@ -1,8 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//using Cysharp.Threading.Tasks;
-//using System;
+using Cysharp.Threading.Tasks;
+using System;
 
 
 namespace CookAppsPxPAssignment.Character.States
@@ -17,7 +17,7 @@ namespace CookAppsPxPAssignment.Character.States
         {
             base.OnEnter();
             _stateMachine.SetAnimationTrigger("Dead");
-            //UnitEnable().Forget();
+            UnitEnable().Forget();
         }
 
         public override void OnExit()
@@ -32,11 +32,11 @@ namespace CookAppsPxPAssignment.Character.States
 
         }
 
-        //private async UniTaskVoid UnitEnable()
-        //{
-        //    await UniTask.Delay(TimeSpan.FromSeconds(1f));
-        //    // 1초 뒤 사라짐
-        //    _stateMachine.Transform.gameObject.SetActive(false);
-        //}
+        private async UniTaskVoid UnitEnable()
+        {
+            await UniTask.Delay(TimeSpan.FromSeconds(1f));
+            // 1초 뒤 사라짐
+            _stateMachine.Transform.gameObject.SetActive(false);
+        }
     }
 }
